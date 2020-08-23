b52ded01c687cc85f50a608a82e81b07b5dc9a1a
Marlon <marlonr@hey.com>
2020-08-23 17:41:47 -0400

    Initial Commit

---
+++ Sticky Header/Sticky_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sticky_HeaderApp.swift
+//  Sticky Header
+//
+//  Created by Marlon Raskin on 8/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Sticky_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
